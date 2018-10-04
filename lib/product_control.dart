5cc2b5d4a34e5656cb2ff1141b63622a853de09d
T1mka <timka2609@gmail.com>
2018-10-04 22:02:28 +0600

    product_control widget updated

--- lib/product_control.dart
+++ lib/product_control.dart
@@ -10,7 +10,7 @@ class ProductControl extends StatelessWidget {
     return RaisedButton(
       color: Theme.of(context).primaryColor,
       onPressed: () {
-        addProduct("Something tasty");
+        addProduct("Something very tasty");
       },
       child: Text("Add Product"),
     );
